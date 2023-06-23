03b842308f0cdc1f64820343358d044d4512d32a
Diego.Carhuavilca <diego.carhuavilca@gmail.com>
2023-06-22 22:49:33 -0500

    init

--- index.js
+++ index.js
@@ -1,14 +1,61 @@
-const http = require('http');
+// Import the functions you need from the SDKs you need
+import { initializeApp, } from "firebase/app";
+import express from 'express';
+import axios from "axios"
 
-const hostname = '0.0.0.0';
-const port = 8080;
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+import {getStorage,ref,getDownloadURL,listAll  } from "firebase/storage"
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDCQTM57smRvpYY0jqs0Xnqr49h-lUXRQQ",
+  authDomain: "topicos-proyecto-389400.firebaseapp.com",
+  projectId: "topicos-proyecto-389400",
+  storageBucket: "topicos-proyecto-389400.appspot.com",
+  messagingSenderId: "824902228587",
+  appId: "1:824902228587:web:f868c527f0f2cf2370ef2e"
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig,"gs://topicos-proyecto-389400.appspot.com/");
+const storage = getStorage(firebase);
+const imageRef = ref(storage,"020837f4-dcf1-49cf-a4fd-1c751c0ae716 (1).png")
+
+
+const app = express();
+
+app.get("/",async (req,res)=>{
+  console.log((await listAll(ref(storage,"cats"))).items.l)
+  res.send("asd")
+})
+
+app.get('/cats',(req, res) => {
+  getDownloadURL(imageRef).then(async (url)=>{
+    const imageResponse = await axios.get(url,{responseType : "stream"})
+    res.setHeader("Content-Type", imageResponse.headers["content-type"])
+    res.setHeader("Content-Length",imageResponse.headers["content-length"])
+    imageResponse.data.pipe(res)
+  }).catch((err)=>{
+    console.log(err)
+    res.send(err)
+  })
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.get('/cheems',(req, res) => {
+  getDownloadURL(imageRef).then(async (url)=>{
+    const imageResponse = await axios.get(url,{responseType : "stream"})
+    res.setHeader("Content-Type", imageResponse.headers["content-type"])
+    res.setHeader("Content-Length",imageResponse.headers["content-length"])
+    imageResponse.data.pipe(res)
+  }).catch((err)=>{
+    console.log(err)
+    res.send(err)
+  })
+});
+
+const port = parseInt(process.env.PORT) || 8080;
+app.listen(port, () => {
+  console.log(`helloworld: listening on port ${port}`);
 });
